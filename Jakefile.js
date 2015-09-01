ae51f720191d1ff59f28fcf9f6b155be2a139a2a
System Administrator <colinsr@me.com>
2015-08-31 22:20:30 -0400

    fixing strange npm issue.

--- Jakefile.js
+++ Jakefile.js
@@ -1,11 +1,10 @@
-/**
- * Created by colinDetwiller on 8/29/15.
- */
-(function(){
-    "use strict";
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
 
-    desc("Default Build");
-    task("default", function(){
-        console.log("\n\nBUILD OKAY");
-    });
+	desc("Default build");
+	task("default", function() {
+		console.log("\n\nBUILD OK");
+	});
+
 }());
